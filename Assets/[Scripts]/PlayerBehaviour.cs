c05e3e77aa21a75603dfcede712e1285232feca0
Sukhmannat Singh <thegamingtitan17@gmail.com>
2024-07-18 13:53:10 -0400

    Added Player Movement

---
+++ Assets/[Scripts]/PlayerBehaviour.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector2 direction = new Vector2(horizontalInput, verticalInput);
+
+        transform.Translate(direction * 5 * Time.deltaTime);
+    }
+}
