616e3bba2924f22513ff8c2017b4067526b645aa
Sukhmannat Singh <thegamingtitan17@gmail.com>
2024-07-19 19:04:49 -0400

    Fix Crafting Bug

--- Assets/[Scripts]/Crafting/Crafting.cs
+++ Assets/[Scripts]/Crafting/Crafting.cs
@@ -6,25 +6,39 @@ using UnityEngine;
 public class Crafting : MonoBehaviour
 {
     public List<Item> pot;
+    public GameObject ban;
+    public GameObject app;
 
     // Start is called before the first frame update
     void Start()
     {
-        pot = new List<Item>();
+        //pot = new List<Item>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.C))
+        if(Input.GetKeyUp(KeyCode.C))
         {
-            pot.Sort();
+            //pot.Sort();
             foreach(Item item in pot[0].item.componentIn)
             {
                 item.item.recipe.Sort();
-                if (pot.SequenceEqual(item.item.recipe)) Debug.Log("crafted");  
+                if (pot.All(item.item.recipe.Contains) && pot.Count == item.item.recipe.Count) Debug.Log("crafted");  
                 else Debug.Log("Not Crafted");
             }            
+        }
+
+        if(Input.GetKeyUp(KeyCode.B))
+        {
+            Instantiate(ban);
+            this.pot.Add(ban.GetComponent<Item>());
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            Instantiate(app);
+            this.pot.Add(app.GetComponent<Item>());
         }
 
     }
