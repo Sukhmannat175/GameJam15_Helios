edce0f9a10c93ceed8408abe02f1dced536a7584
Sukhmannat Singh <thegamingtitan17@gmail.com>
2024-07-24 19:56:08 -0400

    Character Movement and Animations

--- Assets/[Scripts]/Player/PlayerBehaviour.cs
+++ Assets/[Scripts]/Player/PlayerBehaviour.cs
@@ -1,23 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
-{
+{    
+    public float speed = 5f;
+    public Animator anim;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        Movement();
+    }
 
-        Vector2 direction = new Vector2(horizontalInput, verticalInput);
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
 
-        transform.Translate(direction * 5 * Time.deltaTime);
+    private void Movement()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        anim.SetFloat("Horizontal", movement.x);
+        anim.SetFloat("Vertical", movement.y);
+        anim.SetFloat("Speed", movement.sqrMagnitude);
+
+        if(movement.x == 1)
+        {
+            anim.SetBool("Left", false);
+            anim.SetBool("Right", true);
+            anim.SetBool("Up", false);
+            anim.SetBool("Down", false);
+        }
+        if (movement.x == -1)
+        {
+            anim.SetBool("Left", true);
+            anim.SetBool("Right", false);
+            anim.SetBool("Up", false);
+            anim.SetBool("Down", false);
+        }
+        if (movement.y == 1)
+        {
+            anim.SetBool("Left", false);
+            anim.SetBool("Right", false);
+            anim.SetBool("Up", true);
+            anim.SetBool("Down", false);
+        }
+        if (movement.y == -1)
+        {
+            anim.SetBool("Left", false);
+            anim.SetBool("Right", false);
+            anim.SetBool("Up", false);
+            anim.SetBool("Down", true);
+        }
     }
 }
