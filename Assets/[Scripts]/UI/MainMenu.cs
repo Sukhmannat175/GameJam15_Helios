05b549ee68ada43cdcea50b9ab10c123ee8de4c2
Sukhmannat Singh <thegamingtitan17@gmail.com>
2024-07-30 10:13:36 -0400

    Main Menu Done

--- Assets/[Scripts]/UI/MainMenu.cs
+++ Assets/[Scripts]/UI/MainMenu.cs
@@ -6,18 +6,44 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public GameObject optionsMenu;
+    public GameObject tutorial;
+
+    private void Start()
+    {
+        AudioController.Instance.Play("MainMenuBGM");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            optionsMenu.SetActive(false);
+            tutorial.SetActive(false);
+            gameObject.SetActive(true);
+        }
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+        AudioController.Instance.Play("MainMenuClick");
     }
 
     public void OptionsMenu()
     {
         optionsMenu.SetActive(!optionsMenu.activeInHierarchy);
+        AudioController.Instance.Play("MainMenuClick");
+    }
+
+    public void Tutorial()
+    {
+        tutorial.SetActive(!tutorial.activeInHierarchy);
+        AudioController.Instance.Play("MainMenuClick");
     }
 
     public void Quit()
     {
         Application.Quit();
+        AudioController.Instance.Play("MainMenuClick");
     }
 }
